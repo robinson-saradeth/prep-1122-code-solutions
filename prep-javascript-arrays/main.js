debcc4108b00cb614a9df4bd7a967ce6f566e293
Robinson Saradeth <robinsonsaradeth@gmail.com>
2022-11-22 22:25:05 +0000

    Completed prep-javascript-arrays exercise.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,14 @@
+var languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log('languages array', languages);
+
+var firstElement = 'html';
+console.log('the first language of the array is', firstElement);
+
+var thirdElement = 'javascript';
+console.log('the 3rd language of the array is', thirdElement);
+
+var length = languages.length;
+console.log('the length of the array is', length);
+
+var lastElement = languages[languages.length - 1];
+console.log('the last element of the array is', lastElement);
